bac18397658855163de2a23514bc842af8f046f3
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-02-20 20:37:37 +0300

    Add registration Add inline todo comments Add password hashing and hash validation via `passlib` Add temporary `StubDB`

--- src/api/v1.py
+++ src/api/v1.py
@@ -1,28 +1,56 @@
+from http import HTTPStatus
+
 from async_fastapi_jwt_auth import AuthJWT
 from fastapi import Depends, HTTPException, APIRouter
 
+from src.db.stub import stub_database
+from src.models.security import pwd_context
 from src.models.user import User
 
 
 router = APIRouter()
 
 
-@router.post("/login")
+@router.post('/register')
+async def register(user: User):
+    if stub_database.get_data(user.username):
+        raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Username already registered')
+
+    # add password strength check
+    password_hash = pwd_context.hash(user.password)
+
+    stub_database.save_data({user.username: password_hash})  # refactor to service, add DI
+
+    # send registration email
+
+    return {'msg': 'Successfully registered. Verification email send on your email: %s' % user.username}
+
+
+@router.post('/login')
 async def login(user: User, Authorize: AuthJWT = Depends()):
-    if user.username != "test" or user.password != "test":
-        raise HTTPException(status_code=401, detail="Bad username or password")
+    db_user_data = stub_database.get_data(user.username)  # refactor to service, add DI
+
+    if not db_user_data:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Username is not registered')
+    if not pwd_context.verify(user.password, db_user_data.get('password')):
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail='Wrong password')
 
     access_token = await Authorize.create_access_token(subject=user.username)
     refresh_token = await Authorize.create_refresh_token(subject=user.username)
 
     await Authorize.set_access_cookies(access_token)
     await Authorize.set_refresh_cookies(refresh_token)
-    return {"msg": "Successfully login"}
+
+    # rewrite old refresh token by new in Redis
+
+    return {'msg': 'Successfully login'}
 
 
-@router.post("/refresh")
+@router.post('/refresh')
 async def refresh(Authorize: AuthJWT = Depends()):
     await Authorize.jwt_refresh_token_required()
+    # add token expiration validation (or it is already valid?)
+    # check token in Redis
 
     current_user = await Authorize.get_jwt_subject()
 
@@ -32,4 +60,5 @@ async def refresh(Authorize: AuthJWT = Depends()):
     await Authorize.set_access_cookies(new_access_token)
     await Authorize.set_refresh_cookies(new_refresh_token)
 
-    return {"msg": "The token has been refresh"}
+    # rewrite old refresh token by new in Redis
+    return {'msg': 'The token has been refreshed'}
---
+++ src/models/security.py
@@ -0,0 +1,7 @@
+from passlib.context import CryptContext
+
+# add restoring CryptContext (?)
+pwd_context = CryptContext(
+    schemes=["bcrypt",],
+    deprecated="auto",
+)
