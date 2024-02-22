5b7ff1dda9449e6d67e681ad4ea0c5542f8f924b
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-02-22 12:17:39 +0300

    Перенёс многие части кода в отдельный сервис authorization.py. Пока ещё очень многое не работает... Зато кэширование идеально.

--- src/api/v1.py
+++ src/api/v1.py
@@ -1,14 +1,12 @@
-from typing import Annotated, Any
+from typing import Annotated
 
 from async_fastapi_jwt_auth import AuthJWT
 from fastapi import Depends, HTTPException, APIRouter, status
-from redis.asyncio import Redis
 
-from src.db.redis import get_redis
 from src.db.stub import stub_database
 from src.schemas import UserSchema
 from src.schemas.responses import DetailResponse
-from src.services.authorization import get_authorization_service
+from src.services.authorization import get_authorization_service, AuthorizationService, get_fastapi_auth
 from src.utils import passwords
 from src.utils.passwords import verify
 
@@ -52,7 +50,7 @@ async def register(user: UserSchema):
         status.HTTP_404_NOT_FOUND: {'model': DetailResponse, 'description': 'Username is not registered'},
     },
 )
-async def login(user: UserSchema, auth: Annotated[Any, Depends(get_authorization_service)]):
+async def login(user: UserSchema, auth: Annotated[AuthorizationService, Depends(get_authorization_service)]):
     db_user_data = stub_database.get_data('username', user.username)  # refactor to service, add DI
 
     if not db_user_data:
@@ -61,36 +59,32 @@ async def login(user: UserSchema, auth: Annotated[Any, Depends(get_authorization
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Wrong password')
 
     # add role payload to access token
-    await auth.new_token_pair(db_user_data['id'])
+    _, refresh_token = await auth.new_token_pair(db_user_data['id'])
+
+    await auth.save_refresh_token_to_redis(refresh_token)
 
     return {'detail': 'Successfully login'}
 
 
 @router.post('/refresh', response_model=DetailResponse)
-async def refresh(redis: Annotated[Redis, Depends(get_redis)], auth_jwt: Annotated[AuthJWT, Depends(AuthJWT)]):
-    await auth_jwt.jwt_refresh_token_required()
+async def refresh(
+    auth: Annotated[AuthorizationService, Depends(get_authorization_service)],
+    fastapi_auth: AuthJWT = Depends(),
+):
+    await fastapi_auth.jwt_refresh_token_required()
 
-    current_user_id = await auth_jwt.get_jwt_subject()
+    current_user_id = await fastapi_auth.get_jwt_subject()
 
-    new_access_token = await auth_jwt.create_access_token(subject=current_user_id)
-    new_refresh_token = await auth_jwt.create_refresh_token(subject=current_user_id)
-
-    await auth_jwt.set_access_cookies(new_access_token)
-    await auth_jwt.set_refresh_cookies(new_refresh_token)
-
-    # This block marks new refresh token as 'ready' in Redis. Expiration in Redis = token expiration.
-    raw_refresh_token = await auth_jwt.get_raw_jwt()
-    jti = raw_refresh_token['jti']
-    exp = raw_refresh_token['exp']
-    await redis.set(name=jti, value='ready', exat=exp)
+    _, refresh_token = await auth.new_token_pair(current_user_id)
+    await auth.save_refresh_token_to_redis(refresh_token)
 
     return {'detail': 'The token has been refreshed'}
 
 
 @router.post('/logout', response_model=DetailResponse)
-async def logout(auth_jwt: Annotated[AuthJWT, Depends(AuthJWT)]):
-    await auth_jwt.jwt_refresh_token_required()
+async def logout(fastapi_auth: AuthJWT = Depends(),):
+    await fastapi_auth.jwt_refresh_token_required()
 
-    await auth_jwt.unset_jwt_cookies()
+    await fastapi_auth.unset_jwt_cookies()
 
     return {'detail': 'Successfully log out'}
--- src/services/authorization.py
+++ src/services/authorization.py
@@ -1,8 +1,16 @@
 from async_fastapi_jwt_auth import AuthJWT
 from fastapi import Depends
+from pydantic import BaseModel, Field
 from redis.asyncio import Redis
 
 from src.db.redis import get_redis
+
+
+class RedisTokenModel(BaseModel):
+    user_id: str = Field(validation_alias='sub')
+    jti: str = Field(validation_alias='jti')
+    expires_at: int = Field(validation_alias='exp')
+    state: str = 'ready'
 
 
 class AuthorizationService:
@@ -10,70 +18,71 @@ class AuthorizationService:
         self.redis: Redis = redis
         self.auth_jwt: AuthJWT = auth_jwt
 
-    async def new_token_pair(self, user_id):
-        # await self.refresh_token_set_used()
+    @AuthJWT.token_in_denylist_loader
+    async def check_if_refresh_token_in_denylist(self, decrypted_token) -> bool:
+        """
+        Callback which checks if refresh token in the list of used tokens.
 
+        If refresh token correct it will, its status on Redis becomes 'used'.
+
+        If callback result():
+            False -> The user gets access to the endpoint where the refresh token is required.
+            True -> The user has no valid refresh token in cookies. Access is not allowed.
+
+        Each key in Redis is a User UUID.
+        Each value can get one of three states:
+            None: A token is not in Redis. Expected callback result = True.
+            'used': A token already was used. Expected callback result = True. (attacker/user met each other).
+            'ready': A token is ready to use. Expected callback result = False.
+
+        Returns:
+            False: If token JTI == 'ready'.
+            True: In any other cases.
+        """
+        token_model = RedisTokenModel.model_validate(decrypted_token)
+        token_record = await self.redis.hget(name='redis_index', key=token_model.user_id)
+
+        if 'ready' in token_record:
+            token_model.state = 'used'
+            await self.save_refresh_token_to_redis(token_model)
+            return False
+        return True
+
+    async def new_token_pair(self, user_id: str):
+        """Create new access and refresh tokens. Write to cookies."""
         new_access_token = await self.auth_jwt.create_access_token(subject=user_id)
         new_refresh_token = await self.auth_jwt.create_refresh_token(subject=user_id)
 
         await self.auth_jwt.set_access_cookies(new_access_token)
         await self.auth_jwt.set_refresh_cookies(new_refresh_token)
 
-        await self.refresh_token_set_ready(token=new_refresh_token)
+        return new_access_token, new_refresh_token
 
-    # async def refresh_token_set_used(self):
-    #     # await self.auth_jwt.get_jwt_subject()
-    #     self.auth_jwt.get_jti()
-    #     raw_refresh_token = await self.auth_jwt.get_raw_jwt()
-    #     if not raw_refresh_token:
-    #         return None
-    #     jti = raw_refresh_token['jti']
-    #     await self.redis.set(name=jti, value='used', keepttl=True)
+    async def save_refresh_token_to_redis(self, token_data: str | dict | RedisTokenModel):
+        """Save refresh token to Redis. Available formats: [str, dict, RedisTokenModel]"""
+        if type(token_data) is str:
+            token_data = await self.encoded_to_raw_jwt(token_data)
 
-    async def refresh_token_set_ready(self, token=None):
-        raw_refresh_token = await self.auth_jwt.get_raw_jwt(token)
-        jti = raw_refresh_token['jti']
-        exp = raw_refresh_token['exp']
-        await self.redis.set(name=jti, value='ready', exat=exp)
+        if type(token_data) is dict:
+            token_data = await self.raw_jwt_to_model(token_data)
+
+        record_id = token_data.user_id
+        record_data: dict = token_data.model_dump(exclude={'user_id'})
+
+        await self.redis.hset(name=record_id, mapping=record_data)
+
+    async def raw_jwt_to_model(self, raw_jwt_token: dict):
+        """Convert raw jwt token to Redis model."""
+        return RedisTokenModel.model_validate(raw_jwt_token)
+
+    async def encoded_to_raw_jwt(self, encoded_token: str):
+        """Convert encoded token to raw jwt token."""
+        return await self.auth_jwt.get_raw_jwt(encoded_token)
 
 
 async def get_authorization_service(redis: Redis = Depends(get_redis), auth_jwt: AuthJWT = Depends(AuthJWT)):
     return AuthorizationService(redis, auth_jwt)
 
 
-# @AuthJWT.token_in_denylist_loader
-# async def check_if_refresh_token_in_denylist(
-#     decrypted_token: dict, redis: Annotated[Redis, Depends(get_redis)]
-# ) -> bool:
-#     """
-#     Callback which checks if refresh token in the list of used tokens.
-#     Refresh token intended for single use. So used marks as 'used'.
-#
-#     If callback result():
-#         False -> The user gets one-time accesses to the endpoint where the refresh token is required.
-#         True -> The user has no valid refresh token in cookies. Access is not allowed.
-#
-#     Each key in Redis is a JTI (=unique ID) of refresh token.
-#     Each value can get one of three states:
-#         None: A token is not in Redis. Expected callback result = True.
-#             Possible when submitted token is fake.
-#         'used': A token already was used. Expected callback result = True.
-#             Possible when user (or attacker) is trying to reuse token.
-#         'ready': A token is ready to use. Expected callback result = False.
-#             Possible when user has correct refresh token.
-#
-#     Returns:
-#         False: If token JTI == 'ready'.
-#         True: In any other cases.
-#     """
-#     jti = decrypted_token['jti']
-#
-#     token_status = await redis.get(jti)
-#     if token_status == 'ready':
-#         return False
-#     return True
-#     # if token_status == 'ready':
-#     #     await redis.set(name=jti, value='used', keepttl=True)
-#     #     return False
-#     #
-#     # return True
+async def get_fastapi_auth():
+    return AuthJWT
