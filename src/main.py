2b7a52e1c015c3c73aeed6234071f830bb76987d
Maxim Isaev <isaev_0602@mail.ru>
2024-02-25 04:34:57 +0500

    fix

--- src/main.py
+++ src/main.py
@@ -11,11 +11,6 @@ from src.core.config import Settings
 app = FastAPI()
 
 
-@AuthJWT.load_config
-def get_settings():
-    return Settings()
-
-
 @app.exception_handler(AuthJWTException)  # add orjson to tweak performance
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
     return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
