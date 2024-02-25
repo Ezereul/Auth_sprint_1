7bc8cbdea405e9541fcc539ea594f4b8f0fd0384
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-02-25 12:41:40 +0300

    Add docstrings for User schemas

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -2,16 +2,20 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
+    """Base model for User."""
     username: str
 
 
 class UserDB(UserBase):
+    """Output model for registration handler."""
     pass
 
 
 class UserLogin(UserBase):
+    """Input model for login handler."""
     password: str
 
 
 class UserCreateOrUpdate(UserLogin):
+    """Input model for registration handler."""
     pass
