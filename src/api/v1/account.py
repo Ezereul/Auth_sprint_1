8e768048ebedab6bda3b55b3b8e16a53a99ac1a2
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-02-27 18:21:47 +0300

    Finish review fix
    - [x] Session as parameter instead of class init

--- src/api/v1/account.py
+++ src/api/v1/account.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.core.db import get_async_session
 from src.schemas.responses import DetailResponse
 from src.services.account import AccountService, get_account_service
 from src.services.authentication import AuthenticationService, get_authentication_service
@@ -18,13 +20,14 @@ account_router = APIRouter(tags=['Account'])
 async def change_username(
     new_username: str,
     auth: AuthenticationService = Depends(get_authentication_service),
+    db_session: AsyncSession = Depends(get_async_session),
     account: AccountService = Depends(get_account_service),
 ):
     await auth.jwt_required()
 
     user_id = await auth.get_jwt_subject()
 
-    await account.change_username(user_id, new_username)
+    await account.change_username(db_session, user_id, new_username)
 
     return {'detail': 'Username changed'}
 
@@ -41,12 +44,13 @@ async def change_password(
     old_password: str,
     new_password: str,
     auth: AuthenticationService = Depends(get_authentication_service),
+    db_session: AsyncSession = Depends(get_async_session),
     account: AccountService = Depends(get_account_service),
 ):
     await auth.jwt_required()
 
     user_id = await auth.get_jwt_subject()
 
-    await account.change_password(user_id, old_password, new_password)
+    await account.change_password(db_session, user_id, old_password, new_password)
 
     return {'detail': 'Password changed'}
--- src/services/account.py
+++ src/services/account.py
@@ -13,27 +13,25 @@ class AccountDataException(ValueError):
 
 
 class AccountService:
-    def __init__(self, db_session: AsyncSession):
-        self.db_session = db_session
 
-    async def query_user_by_id(self, user_id):
+    async def query_user_by_id(self, db_session: AsyncSession, user_id: str):
         stmt = select(User).where(User.id == user_id)  # noqa
 
-        if not (user := (await self.db_session.scalars(stmt)).first()):
+        if not (user := (await db_session.scalars(stmt)).first()):
             raise AccountDataException('User not found')
         return user
 
-    async def change_username(self, user_id, new_username):
-        user = await self.query_user_by_id(user_id)
+    async def change_username(self, db_session: AsyncSession, user_id: str, new_username: str):
+        user = await self.query_user_by_id(db_session, user_id)
 
         if new_username == user.username:
             raise AccountDataException('New username must be different')
         user.username = new_username
 
-        await self.db_session.commit()
+        await db_session.commit()
 
-    async def change_password(self, user_id, old_password, new_password):
-        user = await self.query_user_by_id(user_id)
+    async def change_password(self, db_session: AsyncSession, user_id: str, old_password: str, new_password: str):
+        user = await self.query_user_by_id(db_session, user_id)
 
         if not user.is_correct_password(old_password):
             raise AccountDataException('Wrong password')
@@ -42,9 +40,8 @@ class AccountService:
 
         user.password = new_password
 
-        await self.db_session.commit()
-
+        await db_session.commit()
 
 @lru_cache
-def get_account_service(db_session: AsyncSession = Depends(get_async_session)):
-    return AccountService(db_session)
+def get_account_service():
+    return AccountService()
