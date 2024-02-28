72e3483ff22b2937faf5c85908144729d2f591a4
Ilya <79277521+LuchnikKek@users.noreply.github.com>
2024-02-28 08:36:40 +0300

    Feature: username and password change (#17)
    * Add docstrings for User schemas
    
    * Add handlers to change username and password:
    Handler `/account/change_username` requires new username. Validates new username and set.
    Handler `/account/change_password` requires old password and new password. Validates new password and set.
    Validation moved to User class.
    
    * For `/change_username` and `/change_password` handlers:
    - [x] Error code 400 instead of 403
    - [x] AuthenticationService instead of AuthJWT
    
    Simple account service fix.
    - [x] Method `query_user_by_id()` to get user by id.
    - [x] Change HTTPException on custom exception for decoupling.
    - [ ] Session as parameter instead of class init
    
    * Finish review fix
    
    - [x] Session as parameter instead of class init

---
+++ src/api/errors.py
@@ -0,0 +1,14 @@
+from async_fastapi_jwt_auth.exceptions import AuthJWTException
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_400_BAD_REQUEST
+
+from src.services.account import AccountDataException
+
+
+def authjwt_exception_handler(_: Request, exc: AuthJWTException):
+    return JSONResponse(status_code=exc.status_code, content={'detail': exc.message})
+
+
+def account_exception_handler(_: Request, exc: AccountDataException):
+    return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={'detail': str(exc)})
--- src/services/users.py
+++ src/services/users.py
@@ -8,11 +8,6 @@ from src.models.user import User
 
 class UserService:
     async def create(self, username: str, password: str, session: AsyncSession):
-        if len(password) < 8:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Password length must be >= 8')
-        if password == username:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Password cannot be equal to login')
-
         if user := (await session.scalars(select(User).where(User.username == username))).first():  # noqa
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Username already registered')
 
